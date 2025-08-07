c390380a82a44504270c178fd665b3b17bd85d74
Strayder90 <strayder@live.com>
2025-08-07 19:04:56 +0200

    Installed required libraries for testing. Added dummy test

---
+++ vitest.config.ts
@@ -0,0 +1,27 @@
+import {defineConfig} from 'vitest/config';
+import {resolve} from 'path';
+
+export default defineConfig({
+    resolve: {
+        alias: {
+            '@/': resolve(__dirname, 'src/') + '/',
+            '@modules/': resolve(__dirname, 'src/modules/') + '/',
+            '@appComponents/': resolve(__dirname, 'src/modules/app/components/') + '/',
+            '@constants/': resolve(__dirname, 'src/modules/app/constants/') + '/',
+            '@utils/': resolve(__dirname, 'src/utils/') + '/'
+        },
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+    },
+    test: {
+        globals: true,
+        environment: 'happy-dom',
+        coverage: {
+            provider: 'v8',
+            reporter: ['text', 'html', 'json'],
+            lines: 90,
+            functions: 90,
+            branches: 90,
+            statements: 90
+        }
+    }
+});
